ffddb364fa3e2c196af53f849c748128a62e199e
sidhu-sukhbhag <156356427+sidhu-sukhbhag@users.noreply.github.com>
2024-06-04 15:41:00 -0400

    changes
    changes

--- js/main.js
+++ js/main.js
@@ -1,9 +1,2 @@
 console.log("Javascript File is Linked")
 
-document.getElementById('lemon-icon').addEventListener('click', function() {
-    console.log('Lemon icon clicked');
-});
-
-document.getElementById('apple-icon').addEventListener('click', function() {
-    console.log('Apple icon clicked');
-}); 
